381ddf95519fd64f3ee479b6c3ab7fa582991cff
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-08 14:09:22 +0100

    add test

--- __test__/route.spec.ts
+++ __test__/route.spec.ts
@@ -1,9 +1,9 @@
 import supertest from 'supertest';
 import app from '../src/app';
 
-describe('GET /balance/:accountNumber', () => {
-  it('should return a 200 response', async () => {
-    const response = await supertest(app).get('/balance/715498694418');
+describe('GET /', () => {
+  it('should return 200 OK', async () => {
+    const response = await supertest(app).get('/');
     expect(response.status).toBe(200);
   });
 });
