a79e4bef2dbfadf816118b98c7b23bb45a0cebfb
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-08 17:15:53 +0100

    update types for typescript

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,14 @@
 export interface Transactions {
-  reference: string
-  senderAccountNumber: string
-  amount: number
-  receiverAccountNumber: string
-  transferDescription: string
-  createdAt: Date
+  reference: string;
+  from: string;
+  amount: number;
+  to: string;
+  transferDescription: string;
+  createdAt: Date;
 }
 
 export interface Balances {
-  accountNumber: string
-  balance: number
-  createdAt: Date
+  accountNumber: string;
+  balance: number;
+  createdAt: Date;
 }
