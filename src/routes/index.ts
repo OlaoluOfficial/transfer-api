d77660716493c2cf3af20f2a53f734668608cb46
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-04-08 16:55:55 +0100

    update name

---
+++ src/routes/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+const router = express.Router();
+import {
+  createNewAccount,
+  getAccount,
+  getTransactions,
+  getBalanaces,
+  makeTransaction,
+} from '../controllers';
+
+router.post('/create-account', createNewAccount);
+
+router.get('/balance/:accountNumber', getAccount);
+
+router.get('/transactions', getTransactions);
+
+router.get('/balance', getBalanaces);
+
+router.post('/transfer', makeTransaction);
+
+export default router;
